b9d8e578533d3e060d0823f0208354c297503b8c
eeshan-vaghjiani <evaghjiani@gmail.com>
2026-08-12 13:25:55 +0300

    feat(storage): add library id generators

--- script.js
+++ script.js
@@ -5,4 +5,13 @@ class LibraryManager {
         this.books = [];
         this.members = [];
     }
+
+    generateId() {
+        return 'BOOK' + Date.now() + Math.random().toString(36).substr(2, 9);
+    }
+
+    generateMemberId() {
+        const count = this.members.length + 1;
+        return 'MEM' + String(count).padStart(3, '0');
+    }
 }
